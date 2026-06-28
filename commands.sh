fa2909f504b294e96ba146dbde28cc783276df4d
Sahit <sahit.singh1@gmail.com>
2026-06-28 17:34:34 +0000

    stylizing

---
+++ commands.sh
@@ -0,0 +1,5 @@
+pandoc modern-cpp-for-python-programmers.md book-style.yaml \
+  -o modern-cpp-for-python-programmers.pdf \
+  --pdf-engine=xelatex \
+  --template=eisvogel \
+  --highlight-style=tango
